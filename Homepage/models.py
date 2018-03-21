8e709d947eb118afc1b08c6febd7bee512e88f09
sparkakapil <sparkakapil@gmail.com>
2018-03-21 16:04:07 +0530

    Fixed Investor Signup Model

--- Homepage/models.py
+++ Homepage/models.py
@@ -7,6 +7,9 @@ from django.db import models
 class Investor(models.Model):
     Name = models.CharField(max_length=100)
     Email = models.CharField(max_length=100)
+    Username = models.CharField(max_length=100,default="")
+    Contact = models.CharField(max_length=15,default="123456")
+    City = models.CharField(max_length=20,default="Pune")
     Password = models.CharField(max_length=100)
 
 
