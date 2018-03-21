f60b4891ec2464f1d09093f2bcd3d7370f4cc7dc
hus <hus.bohra@gmail.com>
2018-03-21 16:13:11 +0530

    inverstor and organization addon

--- Homepage/models.py
+++ Homepage/models.py
@@ -8,5 +8,30 @@ class Investor(models.Model):
     Name = models.CharField(max_length=100)
     Email = models.CharField(max_length=100)
     Password = models.CharField(max_length=100)
+	
+
+class IndividualAddOn(models.Model):
+    Username = ""
+    BusinessType=models.CharField(max_length=50)
+    StartupsFunded=models.CharField(max_length=5)
+    URL=models.CharField(max_length=50)
+    
+
+class OrganizationAddOn(models.Model):
+    Username=""
+    OrganizationType=models.CharField(max_length=50)
+    URL=models.CharField(max_length=50)
+    YearFounded=models.CharField(max_length=4)
+    Size=models.CharField(max_length=6)
+    BusinessType=models.CharField(max_length=50)
+    Specialites = models.CharField(max_length=500)
+
+
+
+
+
+
+
+
 
 
