4a3caff10650f27420a9157221a1a9929b87b7d9
hus <hus.bohra@gmail.com>
2018-03-27 15:25:13 +0530

    views updated with startups

--- Homepage/views.py
+++ Homepage/views.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render,redirect
 
-from .models import Investor,IndividualAddOn, OrganizationAddOn
+from .models import Investor,IndividualAddOn, OrganizationAddOn, Startup, StartupAddOn
 
 # Create your views here.
 
@@ -156,10 +156,123 @@ def Inv_SetSession(request):
 
 
 
+
 ##########################################################################
         #STARTUPS
 ##########################################################################
 
 def startup(request):
+        if(request.method == "POST" and request.POST.get('Submit') == "Upload"):
+        #Handle File Upload
+                ID = Startup.objects.get(id = int(request.session['Startup_id']))
+                
+                if StartupAddOn.objects.filter(id = request.session['Startup_id']):
+                        std = StartupAddOn.objects.get(Username = ID)
+                else:        
+                        std = StartupAddOn()
+                        std.Username = ID
+                
+                ind.Pic = request.FILES['Pic'] 
+                ind.save() 
+                
+                request.session['Pic'] = ind.Pic.url
+                
+                return redirect("/Startup/") 
+      
+        #login's code
+ 
+        if("Startup_id" in request.session):
+                return render(request,'Homepage/StartupProfile.html')   #!!SOLVED!! Issue must be Investor Home not detail form  
+                
+        if(request.method == "POST" and request.POST.get('Submit') == "Sign Up"):
+                return startup_signup(request)
+
+        elif(request.method=="POST" and request.POST.get('Submit')=="Log In"):
+                return startup_login(request)
+                
+  
+
+        else:   
+                return  render(request,'Homepage/startups_login_signup.html')
+
+
+
+def startup_signup(request):
         
-        return  render(request,'Homepage/startups_login_signup.html')
+        if(Startup.objects.filter(Email=request.POST.get('Email'))):
+                context={
+                        'existmsg':'Email already exists'
+                }
+                return render(request,'Homepage/startups_login_signup.html',context)
+                
+        
+        #UNIQUE MOBILE NUMBER CHECK        
+        if(Startup.objects.filter(Contact=request.POST.get('Contact'))):
+                context={
+                        'existmsg':'Contact already exists'
+                }
+                
+                return render(request,'Homepage/startups_login_signup.html',context)
+
+        #UNIQUE USERNAME CHECK               
+        if(Startup.objects.filter(Username=request.POST.get('Username'))):
+                context={
+                        'existmsg':'Username already exists'
+                }
+                
+                return render(request,'Homepage/investors_login_signup.html',context) 
+        
+        startup=Startup()
+
+
+        startup.Name=request.POST.get()
+        startup.Email=request.POST.get()
+        startup.Username=request.POST.get()
+        startup.Contact=request.POST.get()
+        startup.City=request.POST.get()
+        startup.Password=request.POST.get()
+        startup.save()
+
+        return render(request,'Homepage/startups_login_signup.html')
+
+        pass
+
+
+def startup_login(request):
+        username = request.POST.get('Username')
+        password = request.POST.get('Password')
+        
+        #Logic For Login
+        #if((Username == "Email" || Username == "Contact" || Username == "Email" )&& Password == "Password")
+        login=False
+        if("@" in username and "." in username):
+                if(Startup.objects.filter(Email=username,Password=password)):
+                       Startup_SetSession(request)   
+                        login=True
+
+        if(str(username).isdigit()):
+                if(Startup.objects.filter(Contact=username,Password=password)):
+                        Startup_SetSession(request)
+                        login=True  
+                         
+        else:
+                if(Startup.objects.filter(Username=username,Password=password)):
+                        Startup_SetSession(request)
+                        login=True   
+
+        if login == False:
+                context={
+                        'existmsg':'Credentials Incorrect or Not Registered'
+                }
+                return render(request,'Homepage/startups_login_signup.html',context)    
+        pass    
+
+
+def Startup_SetSession(request):
+        request.session['Startup_id'] = startup.id
+        request.session['Startup_Name'] = startup.Name 
+        redirect("/startup/")
+
+
+def Startup_addon(request):
+        pass 
